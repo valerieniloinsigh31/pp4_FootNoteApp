9d5ab9a0c76fcf33d81d0e1cdfa95f43e985af48
valerie_ni_loinsigh <107855608+valerieniloinsigh31@users.noreply.github.com>
2023-03-11 21:03:00 +0000

    Delete FootNote directory

--- FootNote/env.py
+++
@@ -1,6 +0,0 @@
-import os 
-
-os.environ["DATABASE_URL"]="<postgres://gsrpmaru:QL3fjO8S7oN0aYp4ygrJudCdDGB6a1fM@mel.db.elephantsql.com/gsrpmaru>"
-
-os.environ["SECRET_KEY"]="footnote_app_secretkey"
-
